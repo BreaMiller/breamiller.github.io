f27f1780ef4cdb7aeed230d16912851fa59881c6
Brea Miller <brea.m.miller@gmail.com>
2025-07-26 18:28:50 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -352,7 +352,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         // Animate counter
         let currentCount = 0;
-        const increment = Math.ceil(viewCount / 1);
+        const increment = Math.ceil(viewCount / 19);
         const timer = setInterval(() => {
             currentCount += increment;
             if (currentCount >= viewCount) {
