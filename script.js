16e7d0eccab327d33d5677f25c73973a98d0a997
Brea Miller <brea.m.miller@gmail.com>
2025-07-26 16:49:08 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -328,6 +328,6 @@ function throttle(func, limit) {
 // Apply throttling to scroll events
 window.addEventListener('scroll', throttle(function() {
     // Scroll-based animations here
-}, 100));
+}, 100))
 
 })
