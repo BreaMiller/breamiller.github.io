1415d63e92e9038006642609ceff59ac72ede772
Brea Miller <brea.m.miller@gmail.com>
2025-08-06 15:17:35 -0500

    Updated project-data.js

--- project-data.js
+++ project-data.js
@@ -78,7 +78,7 @@ const projectsData = {
         type: 'double',
         items: [
           {
-            title: 'AI-Generated Textures',
+            title: '',
             text: 'Every pattern was created using advanced AI algorithms, resulting in unique textures never seen before.',
             image: 'https://i.imgur.com/vsrO8IJ.jpeg?auto=compress&cs=tinysrgb&w=600'
           },
