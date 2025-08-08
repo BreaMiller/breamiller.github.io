2609ca7d8baea80ad374184e381c910591c8b446
Brea Miller <brea.m.miller@gmail.com>
2025-08-08 14:02:18 -0500

    Updated project-data.js

--- project-data.js
+++ project-data.js
@@ -265,7 +265,7 @@ const projectsData = {
         type: 'single',
         title: '',
         text: 'Optimizing compelling ads is more efficient than ever with AI-generated content.',
-        video: 'https://i.imgur.com/lRQUd6S.mp4?auto=compress&cs=tinysrgb&w=600'
+        img: 'https://i.imgur.com/RYtMF4t.gif?auto=compress&cs=tinysrgb&w=600'
       }
     ],
     overview: 'Revolutionary approach to advertising using AI to create engaging, personalized content that resonates with audiences.'
