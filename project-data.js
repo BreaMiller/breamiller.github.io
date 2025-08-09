37b681a3492a9118eae0a486ce7f724374750aec
Brea Miller <brea.m.miller@gmail.com>
2025-08-09 18:01:19 -0500

    Updated project-data.js

--- project-data.js
+++ project-data.js
@@ -9,7 +9,7 @@ const projectsData = {
       {
         type: 'single',
         title: '',
-        text: 'The Pneuma Collection represents a revolutionary approach to ready-to-wear fashion, every piece is tailored to the individual.',
+        text: 'Ready-to-wearfashion tailored to the individual.',
         image: 'https://i.imgur.com/b2LNbXF.jpeg?auto=compress&cs=tinysrgb&w=600'
       },
       {
