06de3838d83dca116811c8df6154169c5e43ad5f
Brea Miller <brea.m.miller@gmail.com>
2025-08-09 00:55:29 -0500

    Updated project-data.js

--- project-data.js
+++ project-data.js
@@ -98,7 +98,7 @@ const projectsData = {
   'geneproof': {
     title: 'GeneProof',
     label: '',
-    heroImage: 'https://i.imgur.com/pxnYNKW.png?auto=compress&cs=tinysrgb&w=600',
+    heroImage: 'https://i.imgur.com/GyRDHxC.gif?auto=compress&cs=tinysrgb&w=600',
     description: 'Advanced genetic health tracking application for personalized wellness.',
     sections: [
       {
