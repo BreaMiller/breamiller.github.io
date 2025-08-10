c21d2d52364c27607da7e3483063991cfaa4490b
Brea Miller <brea.m.miller@gmail.com>
2025-08-10 17:13:23 -0500

    Deleted renderBrandScroller.tsx

--- src/components/renderBrandScroller.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import BrandScrollerContent from './components/BrandScroller';
-
-const rootElement = document.getElementById('brand-scroller-root');
-
-if (rootElement) {
-  createRoot(rootElement).render(<BrandScrollerContent />);
-}
