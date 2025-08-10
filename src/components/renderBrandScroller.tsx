c1396ba55eebef21bd5d679a04c8a09627f3835f
Brea Miller <brea.m.miller@gmail.com>
2025-08-10 17:12:35 -0500

    Updated renderBrandScroller.tsx

--- src/components/renderBrandScroller.tsx
+++ src/components/renderBrandScroller.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import BrandScrollerContent from './components/BrandScroller'; // Corrected import path
+import BrandScrollerContent from './components/BrandScroller';
 
 const rootElement = document.getElementById('brand-scroller-root');
 
