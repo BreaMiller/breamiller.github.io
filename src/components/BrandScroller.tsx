e41788e92d88cbce56582bb92a7be20e8392da0f
Brea Miller <brea.m.miller@gmail.com>
2025-08-10 19:05:38 -0500

    Updated BrandScroller.tsx

--- src/components/BrandScroller.tsx
+++ src/components/BrandScroller.tsx
@@ -8,7 +8,7 @@ export const BrandScroller = () => {
       <div className="flex animate-marquee whitespace-nowrap">
         {Array(1).fill(10).map((_, i) => (
           <div key={i} className="flex items-center mx-8">
-            <BsSpotify size={20} className="mx-8" />
+            <BsSpotify size={20} className="mx-2" />
             <span className="text-sm font-medium opacity-70 mx-6"></span>
             
             <BsYoutube size={20} className="mx-2" />
