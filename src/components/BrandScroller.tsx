3fca9d723017c2c8959ab6a7f3ec7081bc197c5d
Brea Miller <brea.m.miller@gmail.com>
2025-08-10 19:38:54 -0500

    Updated BrandScroller.tsx

--- src/components/BrandScroller.tsx
+++ src/components/BrandScroller.tsx
@@ -8,17 +8,17 @@ export const BrandScroller = () => {
       <div className="flex animate-marquee whitespace-nowrap">
         {Array(1).fill(10).map((_, i) => (
           <div key={i} className="flex items-center mx-8">
-            <BsSpotify size={20} className="mx-2" />
-            <span className="text-sm font-medium opacity-70 px-6"></span>
+            <BsSpotify size={20} className="px-9" />
+            <span className="text-sm font-medium opacity-70 mx-6"></span>
             
-            <BsYoutube size={20} className="mx-2" />
-            <span className="text-sm font-medium opacity-70 px-6"></span>
+            <BsYoutube size={20} className="px-9" />
+            <span className="text-sm font-medium opacity-70 mx-6"></span>
             
-            <BsAmazon size={20} className="mx-2" />
-            <span className="text-sm font-medium opacity-70 px-6"></span>
+            <BsAmazon size={20} className="px-9" />
+            <span className="text-sm font-medium opacity-70 mx-6"></span>
             
-            <BsGoogle size={20} className="mx-2" />
-            <span className="text-sm font-medium opacity-70 px-6"></span>
+            <BsGoogle size={20} className="px-9" />
+            <span className="text-sm font-medium opacity-70 mx-6"></span>
           </div>
         ))}
       </div>
