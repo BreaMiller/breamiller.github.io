01e30b2e2debe2dd0e86dcac8107ee91562d3045
Brea Miller <brea.m.miller@gmail.com>
2025-08-10 21:11:33 -0500

    Updated BrandScroller.tsx

--- src/components/BrandScroller.tsx
+++ src/components/BrandScroller.tsx
@@ -2,7 +2,7 @@
 
 import { BsAmazon, BsGoogle, BsSpotify, BsYoutube } from "react-icons/bs";
 import { FaFlickr, FaWordpress } from "react-icons/fa";
-import { SiAdobe, SiGooglegemini, SiClaude } from "react-icons/si";
+import { SiAdobe, SiGooglegemini, SiClaude, SiMake } from "react-icons/si";
 import { FiFigma } from "react-icons/fi";
 
 export const BrandScroller = () => {
@@ -32,7 +32,7 @@ export const BrandScroller = () => {
             <SiClaude size={20} className="mx-2" />
             <span className="text-sm font-medium opacity-70 mx-6"></span>
 
-            <SiAdobe size={20} className="mx-2" />
+            <SiMake size={20} className="mx-2" />
             <span className="text-sm font-medium opacity-70 mx-6"></span>
 
              <SiAdobe size={20} className="mx-2" />
@@ -67,7 +67,7 @@ export const BrandScrollerReverse = () => {
             <SiAdobe size={20} className="mx-2" />
             <span className="text-sm font-medium opacity-70 mx-6"></span>          
             
-            <SiAdobe size={20} className="mx-2" />
+            <SiMake size={20} className="mx-2" />
             <span className="text-sm font-medium opacity-70 mx-6"></span>
             
             <SiClaude size={20} className="mx-2" />
