1f6bbe73a9c278b16b1b853ef5cb6c11955f331d
Brea Miller <brea.m.miller@gmail.com>
2025-09-11 14:51:26 -0500

    Updated project-data.js

--- project-data.js
+++ project-data.js
@@ -2,7 +2,6 @@
 const projectsData = {
   'pneuma-collection': {
     title: 'Pneuma Collection',
-    label: '',
     heroImage: '',
     description: 'The future of ready-to-wear fashion: print on demand.',
     sections: [
